fb979085e08f331291f461a739b528685bd51029
Darby Latham <darby@Darbys-MacBook-Pro.local>
2019-11-03 16:07:33 -0600

    Finished Hmwrk

--- orbit-report/src/app/app.component.ts
+++ orbit-report/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Satellite } from './satellite';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  displayList: Satellite[];
+  sourceList: Satellite[];
   title = 'orbit-report';
+  constructor() {
+    this.displayList = []
+    this.sourceList = [];
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+ 
+    window.fetch(satellitesUrl).then(function(response) {
+       response.json().then(function(data) {
+ 
+          let fetchedSatellites = data.satellites;
+         for (let i = 0; i < fetchedSatellites.length; i++) {
+         let satellite =  new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+          this.sourceList.push(satellite);
+                                                          }
+         this.displayList = this.sourceList.slice(0);
+       }.bind(this));
+    }.bind(this));
+ 
+ }
+
+ search(searchTerm: string): void {
+  let matchingSatellites: Satellite[] = [];
+  searchTerm = searchTerm.toLowerCase();
+  for(let i=0; i < this.sourceList.length; i++) {
+     let name = this.sourceList[i].name.toLowerCase();
+     if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+     }
+  }
+  // assign this.displayList to be the the array of matching satellites
+  // this will cause Angular to re-make the table, but now only containing matches
+  this.displayList = matchingSatellites;
 }
+
+};
+
+
--- orbit-report/src/app/satellite.ts
+++ orbit-report/src/app/satellite.ts
@@ -1,2 +1,21 @@
 export class Satellite {
-}
+    name: string;
+    orbitType: string;
+    type: string;
+    operational: boolean;
+    launchDate: string;
+    
+    constructor(name: string, type: string, launchDate: string, 
+        orbitType: string, operational: boolean) {
+    
+            this.name = name;
+            this.orbitType = orbitType;
+            this.type = type;
+            this.operational = operational;
+            this.launchDate = launchDate;
+        }
+        shouldShowWarning (): boolean { 
+            if (this.type === 'Space Debris') {
+            return true;
+                                            }
+}};
